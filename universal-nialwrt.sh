92d746dcabcc2e030394f3edf2c16f4910622725
nialwrt <nialwrt1@gmail.com>
2025-05-14 08:38:09 +0800

    Update universal-nialwrt.sh

--- universal-nialwrt.sh
+++ universal-nialwrt.sh
@@ -25,9 +25,9 @@ main_menu() {
     read -p "Enter choice [1/2/3]: " choice
 
     case "$choice" in
-        1) distro="openwrt"; repo="https://github.com/openwrt/openwrt.git"; deps="build-essential clang flex bison g++ gawk gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev python3-setuptools rsync swig unzip zlib1g-dev file wget";;
-        2) distro="openwrt-ipq"; repo="https://github.com/qosmio/openwrt-ipq.git"; deps="build-essential clang flex bison g++ gawk gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev python3-setuptools rsync swig unzip zlib1g-dev file wget";;
-        3) distro="immortalwrt"; repo="https://github.com/immortalwrt/immortalwrt.git"; deps="ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache clang cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib g++-multilib git gnutls-dev gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libpython3-dev libreadline-dev libssl-dev libtool libyaml-dev libz-dev lld llvm lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip python3-ply python3-docutils python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev zstd";;
+        1) distro="openwrt"; repo="https://github.com/openwrt/openwrt.git"; deps=("build-essential" "clang" "flex" "bison" "g++" "gawk" "gcc-multilib" "g++-multilib" "gettext" "git" "libncurses5-dev" "libssl-dev" "python3-setuptools" "rsync" "swig" "unzip" "zlib1g-dev" "file" "wget");;
+        2) distro="openwrt-ipq"; repo="https://github.com/qosmio/openwrt-ipq.git"; deps=("build-essential" "clang" "flex" "bison" "g++" "gawk" "gcc-multilib" "g++-multilib" "gettext" "git" "libncurses5-dev" "libssl-dev" "python3-setuptools" "rsync" "swig" "unzip" "zlib1g-dev" "file" "wget");;
+        3) distro="immortalwrt"; repo="https://github.com/immortalwrt/immortalwrt.git"; deps=("ack" "antlr3" "asciidoc" "autoconf" "automake" "autopoint" "binutils" "bison" "build-essential" "bzip2" "ccache" "clang" "cmake" "cpio" "curl" "device-tree-compiler" "ecj" "fastjar" "flex" "gawk" "gettext" "gcc-multilib" "g++-multilib" "git" "gnutls-dev" "gperf" "haveged" "help2man" "intltool" "lib32gcc-s1" "libc6-dev-i386" "libelf-dev" "libglib2.0-dev" "libgmp3-dev" "libltdl-dev" "libmpc-dev" "libmpfr-dev" "libncurses-dev" "libpython3-dev" "libreadline-dev" "libssl-dev" "libtool" "libyaml-dev" "libz-dev" "lld" "llvm" "lrzsz" "mkisofs" "msmtp" "nano" "ninja-build" "p7zip" "p7zip-full" "patch" "pkgconf" "python3" "python3-pip" "python3-ply" "python3-docutils" "python3-pyelftools" "qemu-utils" "re2c" "rsync" "scons" "squashfs-tools" "subversion" "swig" "texinfo" "uglifyjs" "upx-ucl" "unzip" "vim" "wget" "xmlto" "xxd" "zlib1g-dev" "zstd");;
         *) echo -e "${RED}${BOLD}Error:${NC} ${RED}Invalid selection. Exiting.${NC}"; exit 1 ;;
     esac
 }
@@ -191,7 +191,10 @@ else
     # Install build dependencies only for a fresh build
     echo -e "${BLUE}Installing required packages...${NC}"
     sudo apt update -y
-    sudo apt install -y "$deps"
+    for dep in "${deps[@]}"; do
+        echo -e "${BLUE}Installing dependency: ${BOLD}${dep}${NC}"
+        sudo apt install -y "$dep"
+    done
     fresh_build
 fi
 
@@ -199,3 +202,5 @@ fi
 cd ..
 echo -e "${BLUE}Removing this script '${script_file}'...${NC}"
 rm -f "$script_file"
+
+sudo apt update && sudo apt install -y
