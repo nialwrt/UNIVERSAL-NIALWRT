6b6650e9163e8162d37d8a22cb584270a377ccc7
nialwrt <nialwrt1@gmail.com>
2025-05-18 13:57:07 +0800

    Update universal-nialwrt.sh

--- universal-nialwrt.sh
+++ universal-nialwrt.sh
@@ -4,26 +4,22 @@ set -e
 
 script_path="$(realpath "$0")"
 
+# ANSI Colors
 RESET='\033[0m'
 BOLD='\033[1m'
-BLACK='\033[30m'
 RED='\033[31m'
 GREEN='\033[32m'
 YELLOW='\033[33m'
 BLUE='\033[34m'
 MAGENTA='\033[35m'
 CYAN='\033[36m'
-WHITE='\033[37m'
 
-BOLD_BLACK="${BOLD}${BLACK}"
 BOLD_RED="${BOLD}${RED}"
 BOLD_GREEN="${BOLD}${GREEN}"
 BOLD_YELLOW="${BOLD}${YELLOW}"
 BOLD_BLUE="${BOLD}${BLUE}"
 BOLD_MAGENTA="${BOLD}${MAGENTA}"
-BOLD_CYAN="${BOLD}${CYAN}"
-BOLD_WHITE="${BOLD}${WHITE}"
-NC="${RESET}"
+RESET="${RESET}"
 
 trap cleanup EXIT
 
@@ -75,18 +71,10 @@ main_menu() {
 define_deps() {
     case "$distro" in
         immortalwrt)
-            deps=(ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential
-                bzip2 ccache clang cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib
-                g++-multilib git gnutls-dev gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev
-                libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libpython3-dev
-                libreadline-dev libssl-dev libtool libyaml-dev libz-dev lld llvm lrzsz mkisofs msmtp nano
-                ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip python3-ply python3-docutils
-                python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs
-                upx-ucl unzip vim wget xmlto xxd zlib1g-dev zstd)
+            deps=(ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache clang cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib g++-multilib git gnutls-dev gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libpython3-dev libreadline-dev libssl-dev libtool libyaml-dev libz-dev lld llvm lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip python3-ply python3-docutils python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev zstd)
             ;;
         openwrt)
-            deps=(build-essential clang flex bison g++ gawk gcc-multilib g++-multilib gettext git
-                libncurses5-dev libssl-dev python3-setuptools rsync swig unzip zlib1g-dev file wget)
+            deps=(build-essential clang flex bison g++ gawk gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev python3-setuptools rsync swig unzip zlib1g-dev file wget)
             ;;
     esac
 }
@@ -150,6 +138,7 @@ start_build() {
     while true; do
         echo -e "${BOLD_YELLOW}DOWNLOADING SOURCES...${RESET}"
         make download -j"$(nproc)"
+
         echo -e "${BOLD_YELLOW}BUILDING WITH $(nproc) CORES...${RESET}"
         local start=$(date +%s)
 
@@ -158,15 +147,12 @@ start_build() {
             printf "${BOLD_GREEN}BUILD COMPLETED IN %02dh %02dm %02ds${RESET}\n" \
                 $((dur / 3600)) $(((dur % 3600) / 60)) $((dur % 60))
             echo -e "${BOLD_BLUE}OUTPUT: $(pwd)/bin/targets/${RESET}"
-
             get_version
-            echo -e "${BOLD_YELLOW}VERSION:${version_branch}${version_tag}${RESET}"
-
+            echo -e "${BOLD_YELLOW}VERSION: ${version_branch}${version_tag}${RESET}"
             break
         else
             echo -e "${BOLD_RED}BUILD FAILED: DEBUGGING WITH VERBOSE OUTPUT${RESET}"
             make -j1 V=s
-
             echo -ne "${BOLD_RED}PLEASE FIX ERROR AND PRESS ENTER TO RETRY${RESET}"
             read -r
             make distclean
@@ -198,7 +184,6 @@ build_menu() {
 rebuild_menu() {
     clear
     cd "$distro" || exit 1
-
     echo -e "${BOLD_MAGENTA}--------------------------------------${RESET}"
     echo -e "${BOLD_MAGENTA}  UNIVERSAL-NIALWRT FIRMWARE BUILD     ${RESET}"
     echo -e "${BOLD_MAGENTA}  https://github.com/nialwrt           ${RESET}"
@@ -223,7 +208,7 @@ rebuild_menu() {
             2)
                 make clean
                 rm -f .config
-                select_target 
+                select_target
                 make defconfig
                 run_menuconfig
                 start_build
@@ -241,6 +226,7 @@ rebuild_menu() {
     done
 }
 
+# MAIN FLOW
 check_git
 main_menu
 define_deps
